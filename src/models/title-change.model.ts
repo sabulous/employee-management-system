7d3a2b441d3afa3c0fb095541f591991bd0a89fa
sabulous <sabsah@gmail.com>
2020-09-01 19:49:46 +0300

    Added TitleChange model.

---
+++ src/models/title-change.model.ts
@@ -0,0 +1,50 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class TitleChange extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  startDate: string;
+
+  @property({
+    type: 'string',
+  })
+  endDate?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  department: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<TitleChange>) {
+    super(data);
+  }
+}
+
+export interface TitleChangeRelations {
+  // describe navigational properties here
+}
+
+export type TitleChangeWithRelations = TitleChange & TitleChangeRelations;
