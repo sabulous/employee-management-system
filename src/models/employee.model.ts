274b97a45c8947d16f7ff698242a2b7003f4403b
sabulous <sabsah@gmail.com>
2020-09-01 17:09:47 +0300

    Added Employee model.

---
+++ src/models/employee.model.ts
@@ -0,0 +1,72 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Employee extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  surname: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+  })
+  phone?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  startdate: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  salary: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'number',
+  })
+  manager?: number;
+
+  @property({
+    type: 'number',
+  })
+  department?: number;
+
+  constructor(data?: Partial<Employee>) {
+    super(data);
+  }
+}
+
+export interface EmployeeRelations {
+  // describe navigational properties here
+}
+
+export type EmployeeWithRelations = Employee & EmployeeRelations;
